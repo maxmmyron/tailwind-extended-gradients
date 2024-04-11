c7e8b606196cbde74936577373f6e9e6f36363fe
maxmmyron <41480159+maxmmyron@users.noreply.github.com>
2024-04-10 21:43:31 -0400

    inherit rotation from rotate theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require("tailwindcss/plugin")
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,ts,svelte}"],
   theme: {
-    gradientDirection: {
+    gradientDirection: ({ theme }) => ({
       t: 'to top',
       tr: 'to top right',
       r: 'to right',
@@ -13,36 +14,15 @@ export default {
       bl: 'to bottom left',
       l: 'to left',
       tl: 'to top left',
-      15: '15deg',
-      30: '30deg',
-      45: '45deg',
-      60: '60deg',
-      75: '75deg',
-      90: '90deg',
-      105: '105deg',
-      120: '120deg',
-      135: '135deg',
-      150: '150deg',
-      165: '165deg',
-      180: '180deg',
-      195: '195deg',
-      210: '210deg',
-      225: '225deg',
-      240: '240deg',
-      255: '255deg',
-      270: '270deg',
-      285: '285deg',
-      300: '300deg',
-      315: '315deg',
-      330: '330deg',
-      345: '345deg',
-    },
+      ...theme('rotate'),
+    }),
     backgroundImage: {
       none: "none",
     },
   },
   corePlugins: {
     backgroundImage: false,
+
   },
   plugins: [
     plugin(function ({ addUtilities, matchUtilities, theme }) {
